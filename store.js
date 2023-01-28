8f60d8f1db2bdeb722e7dcaeced8ebfff91061ec
bin2159 <bipinthms007@gmail.com>
2023-01-28 10:58:34 +0530

    Edit Button

--- store.js
+++ store.js
@@ -11,24 +11,31 @@ function store(e){
         phone:phone
     }
     let myObj_serialized=JSON.stringify(myObj)
-    localStorage.setItem('myObj',myObj_serialized)
-    let myObj_deserialized=JSON.parse(localStorage.getItem('myObj'))
-    console.log(myObj_deserialized)
-    let span=document.createElement('span')
+    localStorage.setItem(myObj.name,myObj_serialized)
+    let myObj_deserialized=JSON.parse(localStorage.getItem(myObj.name))
     let li=document.createElement('li')
-    let input=document.createElement('input')
-    input.type='button'
-    input.value='Delete'
-    span.appendChild(document.createTextNode(myObj_deserialized.name+'----'+myObj_deserialized.email+'----'+myObj_deserialized.phone))
-    li.appendChild(span)
-    li.appendChild(input)
+    let deleteb=document.createElement('input')
+    let edit=document.createElement('input')
+    edit.type='button'
+    edit.value='Edit'
+    deleteb.type='button'
+    deleteb.value='Delete'
+    li.appendChild(document.createTextNode(myObj_deserialized.name+'----'+myObj_deserialized.email+'----'+myObj_deserialized.phone))
+    li.appendChild(deleteb)
+    li.appendChild(edit)
     form.appendChild(li)
     li.addEventListener('click',remove)
     function remove(e){
-        if(e.target.value='delete'){
+        if(e.target.value='Delete'){
                 let item=e.target.parentElement
+                console.log(item)
                 form.removeChild(item)
-                localStorage.removeItem('myObj')
+                localStorage.removeItem(myObj.name)
+        }
+        
+        if(e.target.value='Edit'){
+            let item=e.target.parentElement
+            console.log(document.getElementById('name').value)
         }
     }   
 }
