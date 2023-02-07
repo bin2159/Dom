3251bf1503b845b5b4fe0f22029ceef0f2cd10ae
bin2159 <bipinthms007@gmail.com>
2023-02-08 05:13:27 +0530

    Deleting the Appointments

--- store.js
+++ store.js
@@ -11,16 +11,19 @@ function store(e){
         phone:phone
     }
     
-    axios.post('https://crudcrud.com/api/2fe7f036cbfe4949ae233ec287053f72/data',Obj)
+    axios.post('https://crudcrud.com/api/67233ef4155949f6a1756915c92c5c38/data',Obj)
         .then((res)=>{
             console.log(res)
+            showOnScreen(Obj)
         })
         .catch((err)=>{
             document.body.innerHTML=document.body.innerHTML+'<h4>Something Went Wrong</h4>'
             console.log(err)
         })
-    
-    axios.get('https://crudcrud.com/api/2fe7f036cbfe4949ae233ec287053f72/data')
+}
+getitem()
+function getitem(){
+    axios.get('https://crudcrud.com/api/67233ef4155949f6a1756915c92c5c38/data')
     .then((res)=>{
         console.log(res)
         for(let i=0;i<res.data.length;i++){
@@ -30,8 +33,7 @@ function store(e){
     .catch((err)=>{
         console.log(err)
     })
-
-
+}
     // let myObj_serialized=JSON.stringify(myObj)
     // localStorage.setItem(myObj.email,myObj_serialized)
     // let myObj_deserialized=JSON.parse(localStorage.getItem(myObj.email))
@@ -48,26 +50,33 @@ function store(e){
         li.appendChild(edit)
         form.appendChild(li)
         li.addEventListener('click',remove)
-    }
         function remove(e){
             if(e.target.value='Delete'){
                     let item=e.target.parentElement
                     console.log(item)
+                    axios.delete(`https://crudcrud.com/api/67233ef4155949f6a1756915c92c5c38/data/${myObj._id}`)
+                    .then((res)=>{
+                        console.log(res)
+                    })
+                    .catch((err)=>{
+                        console.log(err)
+                    })
                     form.removeChild(item)
-                    localStorage.removeItem(myObj.email)
             }
             
-            if(e.target.value='Edit'){
-                console.log(myObj)
-                document.getElementById('name').value=myObj.name
-                document.getElementById('email').value=myObj.email
-                document.getElementById('tel').value=myObj.phone
-                let item=e.target.parentElement
-                form.removeChild(item)
-                localStorage.removeItem(myObj.email)
-            }
+            // if(e.target.value='Edit'){
+            //     console.log(myObj)
+            //     document.getElementById('name').value=myObj.name
+            //     document.getElementById('email').value=myObj.email
+            //     document.getElementById('tel').value=myObj.phone
+            //     let item=e.target.parentElement
+            //     form.removeChild(item)
+            //     localStorage.removeItem(myObj.email)
+            // }
         } 
-}
+    }
+        
+
 
 
 
